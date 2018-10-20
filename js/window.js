fe775d7699796503b7b45f726d0c6e1cfa054e18
Brent Oakes <boakes@users.noreply.github.com>
2018-10-20 16:11:40 -0500

    Still not working

--- js/window.js
+++ js/window.js
@@ -2,10 +2,10 @@
 var open = document.getElementsByClassName("window");
 
 for(i=0; i<open.length; ++i){
-	
+
 	document.getElementById(open[i].id).onclick = function(){
 		var tmp = this.id;
-		var closed = document.getElementById(this.id+"-closed");
+		var closed = document.getElementById(tmp+"-closed");
 		
 		this.id = closed.id;
 		closed.id = tmp;
@@ -13,9 +13,12 @@ for(i=0; i<open.length; ++i){
 
 	document.getElementById(open[i].id+"-closed").onclick = function(){
 		var tmp = this.id;
-		var closed = document.getElementById(this.id+"-closed");
-		
+		var closed = document.getElementById(tmp+"-closed");
+		/*
 		this.id = closed.id;
+		closed.id = tmp;
+		*/
+		//TESTING NOW 
 		closed.id = tmp;
 	}
 }
