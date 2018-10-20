1a65abdf7bb70a1632b85bea40809c81a17dfc43
Brent Oakes <boakes@users.noreply.github.com>
2018-10-20 15:04:15 -0500

    Added some window functionality

---
+++ js/window.js
@@ -0,0 +1,21 @@
+
+var open = document.getElementsByClassName("window");
+
+for(i=0; i<open.length; ++i){
+	
+	document.getElementById(open[i].id).onclick = function(){
+		var tmp = this.id;
+		var closed = document.getElementById(this.id+"-closed");
+		
+		this.id = closed.id;
+		closed.id = tmp;
+	}
+
+	document.getElementById(open[i].id+"-closed").onclick = function(){
+		var tmp = this.id;
+		var closed = document.getElementById(this.id+"-closed");
+		
+		this.id = closed.id;
+		closed.id = tmp;
+	}
+}
